6bc7c1257453554b59e427c3d3b9297c09daf9cf
gideonekekeke <Gideonekeke64@gmail.com>
2022-01-23 23:31:30 +0100

    student list button

--- src/Components/Home/LearningComp/MyOwn/Second/Think.js
+++ src/Components/Home/LearningComp/MyOwn/Second/Think.js
@@ -1,60 +1,67 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
-import img from "./img/back.jpg"
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import img from "./img/back.jpg";
+import fil from "../../../Images/3.pdf";
 
 function Think() {
-  return (
-   <Container>
-     <Wrapper>
-       <Allcom>
-         <Title>Think you can change the world with a Degree?  We do!</Title>
-         <Small></Small>
-         <Contentt>
-            We focus on the total transformation of human capacity using qualified
-            team of staff and modern technological devices.
-            <br />
-            We are keen on meeting students expectations and aspirations on record time.
-         </Contentt>
+	return (
+		<Container>
+			<Wrapper>
+				<Allcom>
+					<Title>Think you can change the world with a Degree? We do!</Title>
+					<Small></Small>
+					<Contentt>
+						We focus on the total transformation of human capacity using
+						qualified team of staff and modern technological devices.
+						<br />
+						We are keen on meeting students expectations and aspirations on
+						record time.
+					</Contentt>
 
-        <Link to='/admission' style={{textDecoration:"none"}}>
-          <ButtonCon>
-                EXPLORE PROGRAM OFFERINGS
-          </ButtonCon>
-        </Link>
-       </Allcom>
-     </Wrapper>
-   </Container>
-  )
+					<ButtonCon>
+						<a href={fil} target='_blank'>
+							SEE LIST OF OUR STUDENTS
+						</a>
+					</ButtonCon>
+				</Allcom>
+			</Wrapper>
+		</Container>
+	);
 }
 
-export default Think
+export default Think;
 
 const ButtonCon = styled.div`
-  height: 50px;
-  width: 300px;
-  background-color:#fffeff ;
-  color: #fc1028;
-  text-align: center;
-  cursor: pointer;
-  transform: scale(1);
-  transition: all 350ms;
-  display: flex;
-  justify-content:center;
-  align-items: center;
-  border-radius: 5px;
-  font-weight: 600;
-  margin-top: -20px;
+	height: 50px;
+	width: 300px;
+	background-color: #fffeff;
+	color: #fc1028;
+	text-align: center;
+	cursor: pointer;
+	transform: scale(1);
+	transition: all 350ms;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 5px;
+	font-weight: 600;
 
-&:hover{
-  transform:scale(1.03)
-};
+	margin-top: -20px;
 
-@media screen and (max-width: 425px) {
-  margin-top: 30px;
-}
+	a {
+		text-decoration: none;
+		color: #fc1028;
+	}
 
-`
+	&:hover {
+		transform: scale(1.03);
+	}
+
+	@media screen and (max-width: 425px) {
+		margin-top: 30px;
+	}
+`;
 
 const Contentt = styled.div`
 color:#fffeff;
@@ -67,84 +74,77 @@ font-size: 15px;
   width: 100%;
 }
 }
-`
-
+`;
 
 const Small = styled.div`
-width: 50px;
-height: 10px;
-background-color: #ef9f26;
-/* margin-top: 10px; */
-margin-bottom: 20px;
-`
+	width: 50px;
+	height: 10px;
+	background-color: #ef9f26;
+	/* margin-top: 10px; */
+	margin-bottom: 20px;
+`;
 
 const Title = styled.div`
-color:#fffeff;
-height: 120px;
-width: 600px;
-font-size:35px;
-text-align: center;
-font-weight: 600;
+	color: #fffeff;
+	height: 120px;
+	width: 600px;
+	font-size: 35px;
+	text-align: center;
+	font-weight: 600;
 
+	@media screen and (max-width: 425px) {
+		width: 400px;
+		font-size: 25px;
+	}
 
-@media screen and (max-width:425px){
-  width: 400px;
-  font-size:25px;
-}
+	@media screen and (max-width: 375px) {
+		width: 350px;
+		font-size: 23px;
+		font-weight: 600;
+	}
 
-@media screen and (max-width:375px){
-width: 350px;
-font-size:23px;
-font-weight: 600;
-}
-
-@media screen and (max-width:320px){
-  width: 300px;
-  font-size:20px;
-  font-weight: 600;
-  height: 80px;
-}
-`
+	@media screen and (max-width: 320px) {
+		width: 300px;
+		font-size: 20px;
+		font-weight: 600;
+		height: 80px;
+	}
+`;
 
 const Allcom = styled.div`
-height: 450px;
-width: 700px;
-display: flex;
-flex-direction: column;
-justify-content:center;
-align-items: center;
+	height: 450px;
+	width: 700px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
-
-
-@media screen and (max-width:425px){
-  width: 95%;
-}
-
-`
+	@media screen and (max-width: 425px) {
+		width: 95%;
+	}
+`;
 
 const Container = styled.div`
-width: 100%;
-height: 400px;
-background-image: url(${img});
-background-repeat: repeat-y;
-background-size: cover;
+	width: 100%;
+	height: 400px;
+	background-image: url(${img});
+	background-repeat: repeat-y;
+	background-size: cover;
 
-@media screen and (max-width: 425px){
-  height: 450px
-}
-`
+	@media screen and (max-width: 425px) {
+		height: 450px;
+	}
+`;
 const Wrapper = styled.div`
+	width: 100%;
+	height: 400px;
+	background-color: #fc1028;
+	opacity: 0.9;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-width: 100%;
-height: 400px;
-background-color: #fc1028;
-opacity: 0.9;
-display: flex;
-justify-content:center;
-align-items: center;
-
-@media screen and (max-width: 425px){
-  height: 450px
-}
-
-`
+	@media screen and (max-width: 425px) {
+		height: 450px;
+	}
+`;
